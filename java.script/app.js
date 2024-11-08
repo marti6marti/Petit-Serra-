682d97edf5086397336aa6b943dfdf7113460773
la bona <41652609t@iespoblenou.org>
2024-11-08 09:10:34 +0100

    node +nom

--- java.script/app.js
+++ java.script/app.js
@@ -1,2 +1,5 @@
-console.log("Hello, World!");
-console.log("me cago en t");
+let x = 10;
+let y = 5;
+console.log(x + y); // Sortida: 15
+console.log(x > y); // Sortida: true
+console.log(x === y); // Sortida: false
