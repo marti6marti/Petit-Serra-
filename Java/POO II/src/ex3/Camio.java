3f3ed7fd5d9d39f5f5146b4084a91285bfe08cc3
marti6marti <41652609t@iespoblenou.org>
2024-12-11 12:35:12 +0100

    exercici 3 i 4 de java, poo II

---
+++ Java/POO II/src/ex3/Camio.java
@@ -0,0 +1,38 @@
+package ex3;
+
+
+public class Camio {
+
+    private String matricula;
+    private int pes;
+
+    public Camio(String matricula, int pes)
+    {
+        this.matricula = matricula;
+        this.pes = pes;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getPes() {
+        return pes;
+    }
+
+    public void setPes(int pes) {
+        this.pes = pes;
+    }
+
+    @Override
+    public String toString() {
+        return "Camio{" +
+                "matricula='" + matricula + '\'' +
+                ", pes=" + pes +
+                '}';
+    }
+}
