3d806cde85932e0ecea02224f6e5f256f280680d
marti6marti <41652609t@iespoblenou.org>
2025-01-30 10:14:09 +0100

    juli

--- Java/Strings/src/XifratgeDeJuliusCesar/XifratgeDeJuliusCesar.java
+++ Java/Strings/src/XifratgeDeJuliusCesar/XifratgeDeJuliusCesar.java
@@ -1,7 +1,19 @@
 package XifratgeDeJuliusCesar;
 
+import java.util.Scanner;
+
 public class XifratgeDeJuliusCesar {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        String text = "DAM";
+        int num = 3;
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String noutext = "";
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+
     }
 }
