c5bb40cfada701c9d04bc71745f87a3a44d98b75
marti6marti <41652609t@iespoblenou.org>
2025-03-27 10:13:03 +0100

    projecte 2

---
+++ Java/ProjectePooSolid/src/PersonaExterna.java
@@ -0,0 +1,14 @@
+public class PersonaExterna extends Persona {
+
+
+    public PersonaExterna(String nom, int tel) {
+        super(nom, tel);
+    }
+
+    public String toString() {
+        return "PersonaExterna{" +
+                "nom='" + getNom() + '\'' +
+                ", tel=" + getTel() +
+                '}';
+    }
+}
---
+++ Java/ProjectePooSolid/src/Profesor.java
@@ -0,0 +1,25 @@
+public class Profesor extends Persona{
+    private String materia;
+
+    public Profesor(String nom, int tel, String especialitzacio) {
+        super(nom, tel);
+        this.materia = especialitzacio;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{" +
+                "nom='" + getNom() + '\'' +
+                ", tel=" + getTel() +
+                ", materia='" + materia + '\'' +
+                '}';
+    }
+}
