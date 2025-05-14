44d8c6d1294405b94af11693e2b3e91e8640b334
marti6marti <41652609t@iespoblenou.org>
2025-05-14 12:34:00 +0200

    bbb2

---
+++ Java/PersistenciaBBDD/src/bbdd2/Employee.java
@@ -0,0 +1,61 @@
+package bbdd2;
+
+public class Employee {
+    public int idEmplyee;
+    public String FirtsName;
+    public String LastName;
+    public int PostalCode;
+
+    public Employee(int emplyeeId, String firtName, String lastName, int postalCode) {
+        this.idEmplyee = emplyeeId;
+        FirtsName = firtName;
+        LastName = lastName;
+        PostalCode = postalCode;
+    }
+
+    public Employee(){
+
+    }
+
+    public int getIdEmplyee() {
+        return idEmplyee;
+    }
+
+    public void setIdEmplyee(int idEmplyee) {
+        this.idEmplyee = idEmplyee;
+    }
+
+    public String getFirtsName() {
+        return FirtsName;
+    }
+
+    public void setFirtsName(String firtsName) {
+        FirtsName = firtsName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public int getPostalCode() {
+        return PostalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        PostalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "emplyeeId=" + idEmplyee +
+                ", FirtName='" + FirtsName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", PostalCode=" + PostalCode +
+                '}';
+    }
+}
---
+++ Java/PersistenciaBBDD/src/bbdd2/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package bbdd2;
+
+import java.sql.*;
+import java.util.List;
+
+public interface EmployeeDAO {
+    public int create(Employee employee) throws SQLException;
+    public void delete(int idEmployee) throws SQLException;
+    public Employee read(int idEmployee) throws SQLException;
+    public void update(Employee employee) throws SQLException;
+    public List<Employee> getEmployee() throws SQLException;
+}
